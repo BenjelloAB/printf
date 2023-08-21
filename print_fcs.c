1bd5a67058a60d06b08d99a3a4b03f0a631d060d
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2023-08-21 11:59:00 +0100

    task1_v3

--- print_fcs.c
+++ print_fcs.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * checker_empty - checks for empty slots after %
+ * @s: pointer to the current element in the variadic list
+ * Return: int
+ */
+int checker_empty(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] != ' ')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+/**
  * _printf - clones the printf function in stdio.h
  * @format: pointer holding the string with all formats identifiers and others
  * @...: the list of arguments passed
@@ -10,7 +28,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, count = 0;
+	int i = 0, j = 0, count = 0, si, si2, bo_ol = 0;
 
 	print_t specifiers[] = {
 		{"c", print_char},
@@ -25,16 +43,68 @@ int _printf(const char *format, ...)
 	while (format[i])
 	{
 		j = 0;
+		bo_ol = 0;
+
+		if (format[i] == '%' && format[i + 1] == '\0')
+			break;
+		if (format[i] == '%' && format[i + 1] == ' ')
+		{
+			si2 = checker_empty(&format[i + 1]);
+			if (!si2)
+			{
+				return (count);
+			}
+			else
+			{
+				i += si2 + 1;
+				bo_ol = 1;
+			}
+
+		}
+		if (bo_ol == 1)
+		{
+			while (j < 2)
+			{
+				if (format[i] == *(specifiers[j].symbol))
+				{
+					si = specifiers[j].printer(list, &count);
+					i++;
+					break;
+				}
+				j++;
+			}
+		}
+		if (j < 2 && bo_ol == 1)
+			continue;
+
+		j = 0;
 		while (j < 2)
 		{
 			if (format[i] == '%' && format[i + 1] == *(specifiers[j].symbol))
 			{
-
+				si = specifiers[j].printer(list, &count);
 				i += 2;
 				break;
 			}
 			j++;
 		}
+		if (j < 2)
+			continue;
+		j = 0;
+		while (j < 2)
+		{
+			if (format[i] == '%' && format[i + 1] != *(specifiers[j].symbol))
+			{
+				write(1, &format[i + 1], 1);
+				i += 2;
+				count++;
+				break;
+			}
+			j++;
+		}
+
+		if (j < 2)
+			continue;
 
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
