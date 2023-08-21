f86d7dd6459a82a20a3941f38d0e46fc39d21cd1
root <root@DESKTOP-8KSQ6HV>
2023-08-21 21:57:34 +0100

    0_printf_v1

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+int    counter(const char *s);
+int    print_f(const char *s, ...);
+void print_char(char c, int *count);
+void    print_nbr(int n, int *count);
+int print_str(char *str, int *count);
+void    _printer(const char *s, va_list ptr, int i, int *count);
+
+
+#endif
+
---
+++ print_char.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+void print_char(char c, int *count)
+{
+	write (1, &c, 1);
+	(*count)++;
+}
+
