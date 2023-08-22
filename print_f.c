4b51b6e93b21cf02f109004c7dc3424ef1825dd6
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2023-08-22 01:45:56 +0100

    0task_v7

--- print_f.c
+++ print_f.c
@@ -9,55 +9,57 @@ void _printer(const char *s, va_list ptr, int i, int *count);
  */
 int checker_empty(const char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] != ' ')
-            return (i);
-        i++;
-    }
-    return (-1);
+	while (s[i] != '\0')
+	{
+		if (s[i] != ' ')
+			return (i);
+		i++;
+	}
+	return (-1);
 }
+/**
+ * _printf - clones printf function
+ * @...: list of arguments
+ * @format: pointer to char
+ * Return: int
+ */
 
-int print_f(const char *s, ...)
+int _printf(const char *format, ...)
 {
-    va_list ptr;
-    int i;
-    int count;
-    int sk;
+	va_list ptr;
+	int i, count, sk;
 
-    i = 0;
-    count = 0;
-    if (s == NULL)
-        return (-1);
-
- 
-    va_start(ptr, s);
-    while (s[i])
-    {
-        if (s[i] == '%' && s[i + 1] == '\0')
-            break;
-        if (s[i] == '%' && s[i + 1] == '%')
-        {
-            print_char(s[i], &count);
-            i += 2;
-            continue;
-        }
-        if (s[i] == '%')
-        {
-            sk = checker_empty(&s[i + 1]);
-            if (sk != -1)
-            {
-                i += sk + 1;
-                _printer(s, ptr, i, &count);
-            }
-            break;
-        }
-        else
-            print_char(s[i], &count);
-        i++;
-    }
-    va_end(ptr);
-    return (count);
+	i = 0;
+	count = 0;
+	if (format == NULL)
+		return (-1);
+	va_start(ptr, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			break;
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			print_char(format[i], &count);
+			i += 2;
+			continue;
+		}
+		if (format[i] == '%')
+		{
+			sk = checker_empty(&format[i + 1]);
+			if (sk != -1)
+			{
+				i += sk + 1;
+				_printer(format, ptr, i, &count);
+			}
+			break;
+		}
+		else
+			print_char(format[i], &count);
+		i++;
+	}
+	va_end(ptr);
+	return (count);
 }
