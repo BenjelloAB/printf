f4396ded32e9278f148fc04ad29f379152cf82d3
Khomomo <simolh56@gmail.com>
2023-08-22 21:54:49 +0000

    task0_file1_v1

---
+++ count.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * counter - function
+ * @s: pointer to string
+ * Return: int
+ */
+int counter(const char *s)
+{
+	int i;
+
+	int n;
+
+	i = 0;
+	n = 0;
+	if (s[i] == '%')
+	{
+		n++;
+		i++;
+	}
+	while (s[i])
+	{
+		if (s[i] == '%' && s[i - 1] != '%')
+			n++;
+		i++;
+	}
+	return (n);
+}
