77d21b43c07f3faf305425c9c7bede7a03ae0324
Khomomo <simolh56@gmail.com>
2023-08-22 22:00:30 +0000

    task0_file1_v5

---
+++ print_char.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * print_char - function to print a char
+ * @c: a character
+ * @count: pointer to counter
+ * Return: void
+ */
+void print_char(char c, int *count)
+{
+	write(1, &c, 1);
+	(*count)++;
+}
