f84011c227b7b0d405c9ccb7d5ca95acdcfb5b38
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2023-08-22 16:28:55 +0100

    yask0_v13

--- print_f.c
+++ print_f.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		return (count);
 	}
 	   /*added*/
-	if ((format[0] == '%' && !format[1]) || !format)
+	if (!format)
 		return (-1);
 	if (format[0] == '%' && format[1] == '\0')
 		return (0);
