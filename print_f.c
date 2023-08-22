857ed6e510b68eaffefc17bfe6b3c1a39d58322d
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2023-08-22 20:58:17 +0100

    0task_v17

--- print_f.c
+++ print_f.c
@@ -23,6 +23,8 @@ int _printf(const char *format, ...)
 		return (count);
 	}
 	   /*added*/
+	if (format == NULL)
+		return (-1);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
