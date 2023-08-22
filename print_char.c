f45ee26655249edc333f7683ef12de9334339e3a
Khomomo <simolh56@gmail.com>
2023-08-22 22:03:26 +0000

    main_func_modif_v2

--- print_char.c
+++ print_char.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 /**
- * print_char - function to print a char
- * @c: a character
- * @count: pointer to counter
- * Return: void
+ * print_char - function to print a char.
+ * @c: a character.
+ * @count: pointer to counter.
+ * Return: void.
  */
 void print_char(char c, int *count)
 {
