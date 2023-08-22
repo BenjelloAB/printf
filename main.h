7aff371581d5f7e6e82935283666a0941d2039f4
Khomomo <simolh56@gmail.com>
2023-08-22 21:56:03 +0000

    task0_file1_v1

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int    counter(const char *s);
+int    _printf(const char *s, ...);
+void print_char(char c, int *count);
+void    print_nbr(int n, int *count);
+int print_str(char *str, int *count);
+int _printer(const char *s, va_list ptr, int *i, int *count);
+
+
+#endif
+
