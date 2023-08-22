b56dc9fe44e10281b47abca8b7c3a081292962db
Khomomo <simolh56@gmail.com>
2023-08-22 21:58:30 +0000

    task0_file1_v3

---
+++ print_f.c
@@ -0,0 +1,62 @@
+#include "main.h"
+int _printer(const char *s, va_list ptr, int *i, int *count);
+
+/**
+ * _printf - clones printf function
+ * @...: list of arguments
+ * @format: pointer to char
+ * Return: int
+ */
+int _printf(const char *format, ...)
+{
+	va_list ptr;
+
+	int i, sk, n, count;
+
+	i = 0;
+	count = 0;
+	if (!format)
+		return (-1);
+	/*added*/
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	if (!format[i])
+		return (0);
+	/*added*/
+	n = counter(format);
+
+	if (n == 0)
+	{
+		print_str((char *)format, &count);
+		return (count);
+	}
+	   /*added*/
+	va_start(ptr, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			print_char(format[i], &count);
+			i += 2;
+			continue;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			sk = _printer(format, ptr, &i, &count);
+			if (sk == -1)
+				return (-1);
+		}
+		else
+			print_char(format[i], &count);
+		i++;
+	}
+	va_end(ptr);
+	return (count);
+}
